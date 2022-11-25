0fedbb5ce5020f98a1f7764f235f950b2cf93456
Hamza El Aidi <helaidi@exomind.fr>
2022-11-25 15:18:09 +0100

    init project

---
+++ Test-FDJ-Clubs/Test-FDJ-Clubs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-FDJ-Clubs
+//
+//  Created by Hamza EL Aidi on 25/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Test-FDJ-Clubs/Test-FDJ-ClubsUITests/Test_FDJ_ClubsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Test_FDJ_ClubsUITestsLaunchTests.swift
+//  Test-FDJ-ClubsUITests
+//
+//  Created by Hamza EL Aidi on 25/11/2022.
+//
+
+import XCTest
+
+final class Test_FDJ_ClubsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
